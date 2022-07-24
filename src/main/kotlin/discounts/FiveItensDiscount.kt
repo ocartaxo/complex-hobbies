42eecd4c72d982a97f99b155aebf3b5601c651f6
Otavio Cartaxo <ocartaxo@gmail.com>
2022-07-24 16:09:10 -0300

    verifica se deve aplicar o desconto

--- src/main/kotlin/discounts/FiveItensDiscount.kt
+++ src/main/kotlin/discounts/FiveItensDiscount.kt
@@ -6,5 +6,5 @@ import java.math.BigDecimal
 class FiveItensDiscount(nextDiscount: Discount?) : Discount(nextDiscount) {
 
     override fun computeDiscount(b: Budget): BigDecimal = b.value.multiply(BigDecimal("0.1"))
-    override fun shouldApply(b: Budget): Boolean = b.intensQuantity > 5
+    override fun shouldApply(b: Budget): Boolean = b.itensQuantity > 5
 }
